423ecd70536cde2ea68f5d02ffd81a6a50bdfd54
jessikim <jessica.kim17@gmail.com>
2022-09-02 09:21:12 -0500

    add an array of arrays of winning combos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const winningCombos = [
+  [board[0], board[1], board[2]], 
+  [board[3], board[4], board[5]], 
+  [board[6], board[7], board[8]], 
+  [board[0], board[3], board[6]], 
+  [board[1], board[4], board[7]], 
+  [board[2], board[5], board[8]], 
+  [board[0], board[4], board[8]], 
+  [board[2], board[4], board[6]]
+]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
